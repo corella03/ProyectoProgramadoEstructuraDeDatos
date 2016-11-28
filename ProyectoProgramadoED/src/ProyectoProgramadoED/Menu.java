e86d282048ade6cfbd7f5fdc0a1c228372adcf0e
Jorge Rojas <Jorge@DESKTOP-UR3K285>
2016-11-27 21:18:25 -0600

    DuodecimoAvanceJ

--- ProyectoProgramadoED/src/ProyectoProgramadoED/Menu.java
+++ ProyectoProgramadoED/src/ProyectoProgramadoED/Menu.java
@@ -5,6 +5,10 @@
  */
 //Paquete
 package ProyectoProgramadoED;
+
+import java.awt.HeadlessException;
+import javax.swing.JOptionPane;
+
 /**
  **
  ** @author Luis Alonso Corella Chaves
@@ -12,8 +16,36 @@ package ProyectoProgramadoED;
  ** @date 2016-10-31- Lunes
  **/
 public class Menu {
-    public void menu()
-    {
-        
+        int opc = 0;
+    public void menu() {
+        boolean varGlobal = true;
+        while (varGlobal == true) {
+            try {
+                boolean volMenu2 = true;
+                while (volMenu2 == true) {
+                    while (opc != 1) {//Opciones del primer enunciado y si es diferente a los parámetros muestra mensaje 
+                        opc = Integer.parseInt(JOptionPane.showInputDialog("Opciones \n1: Para empezar \n2: Para salir"));
+                        if (opc >= 3) {
+                            JOptionPane.showMessageDialog(null, "Dato inválido");
+                        }
+                        if (opc == 2) {
+                            break;
+                        }
+                    }
+                }
+                    } catch (HeadlessException | NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Ingresaste un dato  inválido o cancelaste");
+                int resultado = JOptionPane.showConfirmDialog(null, " Deseas volver a iniciar", "Error", JOptionPane.OK_CANCEL_OPTION);
+                if (resultado == JOptionPane.CLOSED_OPTION) {
+                    varGlobal = false;
+                }
+                if (resultado == JOptionPane.CANCEL_OPTION) {
+                    varGlobal = false;
+                }
+                if (resultado == JOptionPane.OK_OPTION) {
+                    varGlobal = true;
+                }
+            }
+}
     }
 }
